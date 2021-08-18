7acfaa573c1f50d220d16b9baf367201c133823b
NataliyaKarasova <natachak2007@gail.com>
2021-08-18 17:03:33 +0300

    тест

---
+++ src/HelloJava.java
@@ -0,0 +1,22 @@
+public class HelloJava {
+    public static void main(String[] args) {
+
+    int a=10;
+    double b=11.1;
+    char c='C';
+    char c2=81;
+    byte d=70;
+    float f=5.16f;
+    int u=9;
+    double [] array ={1.66,2.77,3.88,4,5,6,7,8,9,10.99};
+    double y=array[0];
+    double x=array[9];
+
+                u=10;
+
+    System.out.println("Hello Java");
+        System.out.println(y+" "+x);
+
+    // write your code here
+}
+}
